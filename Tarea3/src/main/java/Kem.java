dcb44c4a9f5a166be9efcc5b006ff9c5492e0358
Emyowo204 <egaete2023@inf.udec.cl>
2024-05-26 16:39:41 -0400

    Creación de las clases Bilz, Pap y Kem

---
+++ Tarea3/src/main/java/Kem.java
@@ -0,0 +1,18 @@
+/** Una bebida azucarada de sabor a piña, que se puede comprar utilizando dinero
+ * @author Chloe Yañez Lavin
+ * @author Emily Osvaldo Gaete Bobadilla */
+
+public class Kem extends Bebida {
+
+    /** Constructor que guarda el número de serie de la Kem
+     * @param serie Número entero con la serie de la Kem */
+    public Kem(int serie) {
+        super(serie);
+    }
+
+    /** Método para obtener el sabor (nombre) de la Kem
+     * @return String "kem" como su sabor */
+    public String sabor() {
+        return "kem";
+    }
+}
