eb0544127d3866a64bc902ad38103433944259d8
Emyowo204 <egaete2023@inf.udec.cl>
2024-05-29 19:05:36 -0400

    Documentación de la clase Comprador

--- Tarea3/src/main/java/Modelos/Comprador.java
+++ Tarea3/src/main/java/Modelos/Comprador.java
@@ -5,12 +5,23 @@ package Modelos;
  * @author Emily Osvaldo Gaete Bobadilla */
 
 public class Comprador {
+
+    /** Deposito donde se guardan las monedas entregadas como vuelto durante una compra */
     private Deposito<Moneda> billetera;
+
+    /** Valor total de las monedas guardadas en la billetera */
     private int numbilletera;
+
+    /** Valor total del vuelto de una compra */
     private int vuelto;
+
+    /** Deposito donde se guardan las monedas que se utilizarán pagar comprar un producto */
     private Deposito<Moneda> depoPago;
+
+    /** Valor total de las monedas que se utilizarán pagar comprar un producto */
     private int numPago;
 
+    /** Constructor de Comprador donde se inicializan sus propiedades */
     public Comprador() {
         billetera = new Deposito<>();
         numbilletera = 0;
@@ -18,6 +29,9 @@ public class Comprador {
         depoPago = new Deposito<>();
         numPago = 0;
     }
+
+    /** Método para obtener el vuelto de un deposito con monedas
+     * @param depoVuelto El deposito con las monedas entregadas como vuelto */
     public void obtenerVuelto(Deposito<Moneda> depoVuelto) {
         vuelto = 0;
         Moneda monVuelto=null;
@@ -32,21 +46,39 @@ public class Comprador {
             }
         }
     }
+
+    /** Método para establecer el valor total de las monedas que se utilizarán pagar comprar un producto
+     * @param num El número que se igualará al valor total de las monedas del deopisto */
     public void setnumPago(int num) {
         numPago=num;
     }
+
+    /** Método para obtener el valor total del vuelto de una compra
+     * @return Un número con el valor total de vuelto */
     public int getVuelto() {
         return vuelto;
     }
+
+    /** Método para obtener el valor total de las monedas que se utilizarán pagar comprar un producto
+     * @return Un número con el valor total de vuelto */
     public int getnumPago() {
         return numPago;
     }
+
+    /** Método para obtener el deposito donde se guardan las monedas que se utilizarán pagar comprar un producto
+     * @return El deposito con las monedas */
     public Deposito<Moneda> getdepoPago() {
         return depoPago;
     }
+
+    /** Método para obtener el valor total de las monedas guardadas en la billetera
+     * @return Un número con el valor total de las monedas */
     public int getNumBilletera() {
         return numbilletera;
     }
+
+    /** Método para agregar una moneda al deposito que se utilizarán pagar comprar un producto
+     * @param moneda Una moneda que se agregará al deposito de compra */
     public void addMoneda(Moneda moneda) {
         depoPago.addContenido(moneda);
         numPago += moneda.getValor();
--- Tarea3/src/main/java/Vistas/Main.java
+++ Tarea3/src/main/java/Vistas/Main.java
@@ -1,21 +1,11 @@
 package Vistas;
 
-import Vistas.Ventana;
-
 /** El main en donde se realiza el programa
  * @author Chloe Yañez Lavin
  * @author Emily Osvaldo Gaete Bobadilla */
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
         Ventana ventana = new Ventana();
-
-        System.out.println("1100%1000: "+(1000%1000));
-        System.out.println("900%1000: "+(1000%2000));
-        System.out.println("600%500: "+(500%400));
-        System.out.println("400%500: "+(500%700));
-        System.out.println("200%100: "+(100%200));
-
     }
 }
