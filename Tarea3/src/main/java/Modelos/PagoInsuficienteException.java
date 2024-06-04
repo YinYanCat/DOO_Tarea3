d6a241d8a51c3f39ed870dbf08ddf50c375bbedd
Emyowo204 <egaete2023@inf.udec.cl>
2024-06-03 23:17:02 -0400

    Eliminación de codigo antiguo en main

--- Tarea3/src/main/java/Modelos/PagoInsuficienteException.java
+++
@@ -1,9 +0,0 @@
-package Modelos;
-
-/** Esta excepción es resultante de un precio del producto mayor al valor de la moneda entregada */
-public class PagoInsuficienteException extends Exception {
-    public PagoInsuficienteException(String errorMessage) {
-        super(errorMessage);
-    }
-}
-
--- Tarea3/src/main/java/Modelos/ProductoNoRetiradoException.java
+++
@@ -1,9 +0,0 @@
-package Modelos;
-
-/** Esta excepción es resultante de producto sin retirar del expendedor */
-public class ProductoNoRetiradoException extends Exception {
-    public ProductoNoRetiradoException(String errorMessage) {
-        super(errorMessage);
-    }
-}
-
